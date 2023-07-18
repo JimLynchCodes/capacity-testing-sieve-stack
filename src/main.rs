fcf010b4657e8400e5056f57dceb2a1a3b3bb790
JimLynchCodes <mrdotjim@gmail.com>
2023-07-17 21:16:52 -0400

    fix file name error

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ mod custom_types;
 mod send_through_sieve_stack;
 mod unit_test_mocks;
 
+/// Runs the filtering logic locally with a custom csv
 fn main() {
+
+    // TODO - load custom csv
+
+    // TODO - run cvs data through "send_through_sieve_stack" function
+
+    // TODO - print filtered data points
+
     println!("Hello, sieve stack!");
 }

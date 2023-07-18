66a7f45cedbe7b69c262f88152ff0ed01fa1d862
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-07-18 11:02:03 -0400

    remove option in function return

--- src/send_through_sieve_stack.rs
+++ src/send_through_sieve_stack.rs
@@ -2,17 +2,19 @@ use crate::custom_types::CapacityTestDataPoint;
 
 pub fn send_through_sieve_stack(
     _input_data_points: Vec<CapacityTestDataPoint>,
-) -> Option<Vec<CapacityTestDataPoint>> {
-    None
+) -> Vec<CapacityTestDataPoint> {
+    vec![]
 }
 
 #[cfg(test)]
 mod send_through_sieve_stack_tests {
 
-    use crate::{send_through_sieve_stack::send_through_sieve_stack, unit_test_mocks::dummy_input_data_point};
+    use crate::{
+        send_through_sieve_stack::send_through_sieve_stack, unit_test_mocks::dummy_input_data_point,
+    };
 
     #[test]
     fn filters_out_everything() {
-        assert!(send_through_sieve_stack(vec!(dummy_input_data_point())).is_none());
+        assert!(send_through_sieve_stack(vec!(dummy_input_data_point())).is_empty());
     }
 }

48bffaf2b52b39ea622d1cd805df2076a97fd7cf
JimLynchCodes <mrdotjim@gmail.com>
2023-07-30 21:35:27 -0400

    add basic sieves and linear regression lib

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+use std::error::Error;
+
+use crate::{
+    send_through_sieve_stack::send_through_sieve_stack, unit_test_mocks::dummy_input_data_point,
+};
+
 mod custom_types;
 mod send_through_sieve_stack;
 mod unit_test_mocks;
 
 /// Runs the filtering logic locally with a custom csv
+// fn main() -> Result<(), Box<dyn Error>> {
 fn main() {
-
     // TODO - load custom csv
+    let input_points = vec![dummy_input_data_point()];
 
     // TODO - run cvs data through "send_through_sieve_stack" function
+    let filtered_points = send_through_sieve_stack(input_points);
 
     // TODO - print filtered data points
+    println!("{:#?}", filtered_points.get(0).unwrap());
 
     println!("Hello, sieve stack!");
 }
